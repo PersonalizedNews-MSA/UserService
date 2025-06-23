c1046c19c78d97ce32f3be0388b13c7a7f059468
suin <73993670+whl5105@users.noreply.github.com>
2025-06-24 00:52:29 +0900

    feat. probe , dev 파일 수정

---
+++ src/main/java/com/mini2/user_service/service/probe/ProbeService.java
@@ -0,0 +1,18 @@
+package com.mini2.user_service.service.probe;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProbeService {
+
+    public void validateLiveness() {
+    // TODO: 만일 서비스를 재시작 해야 하는 상황이면 exception 발생
+    }
+
+    public void validateReadiness() {
+        // TODO: 만일 서비스 수행일 일시 중지해야 하는 상황이면 exception 발생
+    }
+
+}
